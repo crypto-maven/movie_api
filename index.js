aa4db1bdc88250ff4c3e1f073baa4c0f589e7cea
Nicole Villa <nicolevilla.109@hotmail.com>
2020-01-07 16:56:21 -0600

    movies as array
    10 movies with title and director

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 const express = require('express');
 const app = express();
 
+let topMovies = [
+	{
+		title: 'The Imaginarium of Doctor Parnassus',
+		director: 'Terry Gilliam'
+	},
+	{
+		title: 'The Pianist',
+		director: 'Roman Polanski'
+	},
+	{
+		title: 'Tekkonkinkreet',
+		director: 'Michael Arias'
+	},
+	{
+		title: 'Shaolin Soccer',
+		director: 'Stephen Chow'
+	},
+	{
+		title: 'Dr.Strangelove',
+		director: 'Stanley Kubrick'
+	},
+	{
+		title: '2 Days in the Valley',
+		director: 'John Herzfeld'
+	},
+	{
+		title: 'Very Bad Things',
+		director: 'Peter Berg'
+	},
+	{
+		title: 'Garden State',
+		director: 'Zach Braff'
+	},
+	{
+		title: 'Sweeney Todd',
+		director: 'Tim Burton'
+	},
+	{
+		title: 'Big Fish',
+		director: 'Tim Burton'
+	}
+];
+
 // GET requests
 app.get('/', function(req, res) {
 	res.send('Welcome to myFlix Movies!');
