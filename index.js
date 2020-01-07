c4ae00e80a317dad70dfe1f91f7adf40b9a7f2e9
Nicole Villa <nicolevilla.109@hotmail.com>
2020-01-07 16:18:16 -0600

    index.js
    create get route located at /movies that returns default textual result of your choice

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const app = express();
+
+// GET requests
+app.get('/', function(req, res) {
+	res.send('Welcome to myFlix Movies!');
+});
+app.get('/documentation', function(req, res) {
+	res.sendFile('public/documentation.html', { root: __dirname });
+});
+app.get('/movies', function(req, res) {
+	res.json(topMovies);
+});
+
+// listen for requests
+app.listen(8080, () => console.log('Your app is listening on port 8080.'));
