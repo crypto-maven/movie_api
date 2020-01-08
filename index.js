c20c5ea31ddeb009d3e2dc1fe3b22e628ad1e621
Nicole Villa <nicolevilla.109@hotmail.com>
2020-01-08 14:02:45 -0600

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+morgan = require('morgan');
+
 let topMovies = [
 	{
 		title: 'The Imaginarium of Doctor Parnassus',
@@ -49,7 +51,7 @@ let topMovies = [
 app.use(express.static('public'));
 
 // Welcome message
-app.get('/', (req, res) => {
+app.get('/movies', (req, res) => {
 	res.send('Welcome to a list of 10 Movies!');
 });
 
@@ -59,3 +61,24 @@ app.get('/', (req, res) => {
 
 // listen for requests
 app.listen(8080, () => console.log('Your app is listening on port 8080.'));
+
+// Morgan Middleware
+
+app.use(morgan('common'));
+
+app.get('/', function(req, res) {
+	res.send('Welcome to my app!');
+});
+app.get('/secreturl', function(req, res) {
+	res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080);
+
+// error handling function morgan
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).send('Unexpected Error!');
+	next();
+});
