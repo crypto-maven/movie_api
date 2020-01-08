4df7b8c36d059b5ef450e9fe98e908553c7a6b84
Nicole Villa <nicolevilla.109@hotmail.com>
2020-01-08 13:23:45 -0600

    express static

--- index.js
+++ index.js
@@ -45,15 +45,17 @@ let topMovies = [
 ];
 
 // GET requests
-app.get('/', function(req, res) {
-	res.send('Welcome to myFlix Movies!');
+
+app.use(express.static('public'));
+
+// Welcome message
+app.get('/', (req, res) => {
+	res.send('Welcome to a list of 10 Movies!');
 });
-app.get('/documentation', function(req, res) {
-	res.sendFile('public/documentation.html', { root: __dirname });
-});
-app.get('/movies', function(req, res) {
-	res.json(topMovies);
-});
+
+// app.get('/movies', function(req, res) {
+// 	res.json(topMovies);
+// });
 
 // listen for requests
 app.listen(8080, () => console.log('Your app is listening on port 8080.'));
