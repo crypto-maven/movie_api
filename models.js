49c0ebd9d37c08cac7efc93a26f30991ddd02ec4
crypto-maven <40839717+crypto-maven@users.noreply.github.com>
2020-03-07 13:10:20 -0600

    bcrypt in models.js

--- models.js
+++ models.js
@@ -23,6 +23,26 @@ var userSchema = mongoose.Schema({
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: "Movie"}]
 });
 
+// bcrypt
+const bcrypt = require('bcrypt');
+
+var userSchema = mongoose.Schema({
+  Username : {type: String, required: true},
+  Password : {type: String, required: true},
+  Email : {type: String, required: true},
+  Birthday : Date,
+  FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password); };
+};
+
+
 var Movie = mongoose.model("Movie", movieSchema);
 var User = mongoose.model('User', userSchema);
 
