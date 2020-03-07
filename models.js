1c7993a5f11a1b49b4a273e0d44b823d72aacdba
crypto-maven <40839717+crypto-maven@users.noreply.github.com>
2020-03-07 13:41:31 -0600

    removed duplicate code

--- models.js
+++ models.js
@@ -26,13 +26,6 @@ var userSchema = mongoose.Schema({
 // bcrypt
 const bcrypt = require('bcrypt');
 
-var userSchema = mongoose.Schema({
-  Username : {type: String, required: true},
-  Password : {type: String, required: true},
-  Email : {type: String, required: true},
-  Birthday : Date,
-  FavoriteMovies : [{ type : mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
-});
 
 userSchema.statics.hashPassword = function(password) {
   return bcrypt.hashSync(password, 10);
@@ -40,7 +33,7 @@ userSchema.statics.hashPassword = function(password) {
 
 userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password); };
-};
+
 
 
 var Movie = mongoose.model("Movie", movieSchema);
