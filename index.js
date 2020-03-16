05ae77006863cc29f99c07dc7aad7a70a277dc7c
crypto-maven <40839717+crypto-maven@users.noreply.github.com>
2020-03-16 11:00:30 -0500

    push saved changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const Users = Models.User;
 // mongoose.connect('mongodb://localhost:27017/myFlixDB', {useNewUrlParser: true});
 
 // allowing mongoose to connect to online db via mongodb
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://myFlixDBadmin:B0h2sQvncx@startercluster-piq8s.mongodb.net/myFlixDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // code from previous exercises
 const movies = require('./movies.js');
