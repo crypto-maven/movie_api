b9dfb553d67e77e58b8d89aa91d0bbe8b8498163
crypto-maven <40839717+crypto-maven@users.noreply.github.com>
2020-04-25 20:59:06 -0500

    prop-types module / etc.

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,11 +10,12 @@ import './index.scss';
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
+    
     return (
       <div className="my-flix">
-        <MainView>
-          
-        </MainView>
+        <MainView/>
+        
+        
       </div>
     );
   }
